af79fc82c1bdc5291c76d91e454c07961e0fb07e
William Anderson <carrig212@gmail.com>
2019-10-15 21:13:53 +0100

    Initial commit

--- src/main/java/com/brightflag/domain/Student.java
+++ src/main/java/com/brightflag/domain/Student.java
@@ -1,10 +1,15 @@
 package com.brightflag.domain;
 
+import java.util.List;
+
 public class Student {
 
 	private Integer studentID;
 	private String firstName;
 	private String lastName;
+	private List<Subject> subjects;
+	private List<Exam> exams;
+	private List<Grade> grades;
 
 	public Integer getStudentID() {
 		return studentID;
@@ -30,9 +35,39 @@ public class Student {
 		this.lastName = lastName;
 	}
 
-	@Override
-	public String toString() {
-		return "Student [studentID=" + studentID + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	public List<Subject> getSubjects() {
+		return subjects;
 	}
 
+	public void setSubjects(List<Subject> subjects) {
+		this.subjects = subjects;
+	}
+
+	public List<Exam> getExams() {
+		return exams;
+	}
+
+	public void setExams(List<Exam> exams) {
+		this.exams = exams;
+	}
+
+	public List<Grade> getGrades() {
+		return grades;
+	}
+
+	public void setGrades(List<Grade> grades) {
+		this.grades = grades;
+	}
+
+	@Override
+	public String toString() {
+		return "Student{" +
+				"studentID=" + studentID +
+				", firstName='" + firstName + '\'' +
+				", lastName='" + lastName + '\'' +
+				", subjects=" + subjects +
+				", exams=" + exams +
+				", grades=" + grades +
+				'}';
+	}
 }
--- src/main/java/com/brightflag/domain/Subject.java
+++ src/main/java/com/brightflag/domain/Subject.java
@@ -1,9 +1,12 @@
 package com.brightflag.domain;
 
+import java.util.List;
+
 public class Subject {
 
 	private Integer subjectID;
 	private String subjectName;
+	private List<Student> students;
 
 	public Integer getSubjectID() {
 		return subjectID;
@@ -21,9 +24,20 @@ public class Subject {
 		this.subjectName = subjectName;
 	}
 
-	@Override
-	public String toString() {
-		return "Subject [subjectID=" + subjectID + ", subjectName=" + subjectName + "]";
+	public List<Student> getStudents() {
+		return students;
 	}
 
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	@Override
+	public String toString() {
+		return "Subject{" +
+				"subjectID=" + subjectID +
+				", subjectName='" + subjectName + '\'' +
+				", students=" + students +
+				'}';
+	}
 }
